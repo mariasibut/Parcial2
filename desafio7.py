830b3fd59ab868c6dfd810dd9db62c2aedb8b9fd
mariasibut <maria.sibut.silva@escola.pr.gov.br>
2026-04-08 22:52:16 -0300

    Update desafio7.py

--- desafio7.py
+++ desafio7.py
@@ -1,13 +1,12 @@
-# ===== DESAFIO - JUROS SIMPLES =====
-
-print('===== CÁLCULO DE JUROS SIMPLES =====')
+# Exibe o título 
+print(' DESAFIO 7 - CÁLCULO DE JUROS SIMPLES ')
 
 # Entrada de dados: capital, taxa e tempo
 C = float(input('Digite o capital (C): '))
 I = float(input('Digite a taxa (%): '))
 T = float(input('Digite o tempo: '))
 
-# Processamento: aplica a fórmula J = (C * I * T) / 100
+# Processamento: usa a fórmula J = (C * I * T) / 100
 J = (C * I * T) / 100
 
 # Saída de dados
